02cd0bc68e803e4abeccd7869e09f6b832a96c1b
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2024-08-29 15:16:33 -0500

    ASSETS Q1 P2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+  before_destroy :check_for_bookmarks
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
+
+  def check_for_bookmarks
+    return unless bookmarks.empty?
+    errors.add(:base, "Cannot delete movie")
+    throw(:abort)
+  end
 end
