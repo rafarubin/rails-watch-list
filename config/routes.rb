f2b4a272d94d5e815505acc3542078c2cd78d7c8
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2024-08-30 13:15:05 -0500

    Agregamos un root lists#index para que corra Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
 
   # get "lists", to: "lists#index", as: :lists
   # get "lists/new", to: "lists#new", as: :new_list
